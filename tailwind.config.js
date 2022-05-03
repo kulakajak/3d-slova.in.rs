f7ee116cd312c231d24351508756a96afe337483
milankecman270 <milan.kecman270@gmail.com>
2022-05-03 11:51:54 +0200

    Added Flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   content: [
-    './_drafts/**/*.html',
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/*.md',
-    './*.md',
-    './*.html',
+    "./_drafts/**/*.html",
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.md",
+    "./*.md",
+    "./*.html",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     theme: {
       extend: {},
     },
   },
-  plugins: []
-}
+  plugins: [require("flowbite/plugin")],
+};
